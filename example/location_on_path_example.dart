4039936ab52bfac6642e7d775d7915788efa3ed9
JacoNeto <jaconeto8@gmail.com>
2023-09-15 19:11:43 -0300

    first commit

---
+++ example/location_on_path_example.dart
@@ -0,0 +1,6 @@
+import 'package:location_on_path/location_on_path.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/location_on_path_test.dart
@@ -0,0 +1,16 @@
+import 'package:location_on_path/location_on_path.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
